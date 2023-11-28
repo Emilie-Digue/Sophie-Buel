6510af871d7534b63a19ef3748b7658e3ed780a3
Emilie-Digue <145977765+Emilie-Digue@users.noreply.github.com>
2023-11-28 11:25:15 +0100

    Add files via upload

--- FrontEnd/formulaire.js
+++ FrontEnd/formulaire.js
@@ -1,13 +1,18 @@
-let baliseEmail = document.getElementById("email")
-let email = baliseEmail.value 
-console.log(email);
-let baliseMdp = document.getElementById("password")
-let mdp = baliseMdp.value
-console.log(mdp);
-
 const form = document.querySelector('form');
 
 form.addEventListener("submit", (event)=> {
-    event.preventDefautlt();
+    event.preventDefault();
     console.log("Il ny' a pas eu de rechargment");
+
+    const login = {
+        email : document.querySelector("email"),
+        password: document.querySelector("password"),
+    }
+    const chargeUtile = JSON.stringify(login);
+
+   fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: chargeUtile
+    })
 })

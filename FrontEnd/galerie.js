472f018d12973f498ec0e1ea233b4461cb072180
Emilie-Digue <145977765+Emilie-Digue@users.noreply.github.com>
2023-11-28 11:25:48 +0100

    Add files via upload

--- FrontEnd/galerie.js
+++ FrontEnd/galerie.js
@@ -26,15 +26,62 @@ async function genererProjets() {
 genererProjets();
 
 /* création des filtres*/
-const boutonObjet = document.querySelector(".btn_objets");
-boutonObjet.addEventListener("click", async function() {
-    const reponse = await fetch('http://localhost:5678/api/works');
-    const projets = await reponse.json();
+async function genererFiltre(){
+    const response = await fetch ('http://localhost:5678/api/categories')
+    const categorie = await response.json();
     
-    const objetFiltre = projets.filter(function (projet) {
-        return categoryId = 1;
+    const sectionFiltres = document.querySelector(".filtres");
+    const btnTous = document.createElement("button");
+    btnTous.innerText = "Tous";
+    sectionFiltres.appendChild(btnTous);
+
+    for (let i=0; i <categorie.length; i++) {
+
+        const filtre = categorie[i];
+        const sectionFiltres = document.querySelector(".filtres");
+
+        const button = document.createElement("button");
+        button.innerText = filtre.name;
+
+        sectionFiltres.appendChild(button);
+    
+    button.addEventListener("click", async function() {
+        const reponse = await fetch('http://localhost:5678/api/works');
+        const projets = await reponse.json();
+        
+        for (let i=0; i < projets.length; i++) {
+            const article = projets[i];
+            const sectionFiches = document.querySelector(".gallery");
+           
+            const projetElement = document.createElement("article");
+            const imageElement = document.createElement("img");
+           
+            const nomElement = document.createElement("figcaption");
+            nomElement.innerText = "";
+    
+            sectionFiches.appendChild(projetElement);
+            projetElement.appendChild(imageElement);
+            projetElement.appendChild(nomElement);    
+            
+        const objetFiltre = projets.filter(function (projet) {
+
+        const articleFiltre = categorie[i];
+        const sectionFiches = document.querySelector(".gallery");
+       
+        const projetElement = document.createElement("article");
+        const imageElement = document.createElement("img");
+        imageElement.src = article.imageUrl;
+        const nomElement = document.createElement("figcaption");
+        nomElement.innerText = article.title;
+
+        sectionFiches.appendChild(projetElement);
+        projetElement.appendChild(imageElement);
+        projetElement.appendChild(nomElement);;
+        })
+        console.log(objetFiltre);
+    }
     })
-    console.log(objetFiltre)
-});
+}
+}
 
-
+genererFiltre();

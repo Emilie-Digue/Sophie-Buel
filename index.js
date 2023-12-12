f8f6313818d45aab9b2c6d7d4769c976372e6fde
Emilie-Digue <145977765+Emilie-Digue@users.noreply.github.com>
2023-12-12 14:11:05 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/* Mode d'affichage */
+const reponse = await fetch("http://localhost:5678/api/users/login")
+const token = await reponse.json();
+
+function modeAdmin() {
+    if (token !=== null) {
+        const admin = document.querySelectorAll(".edition");
+        admin.style.display = block;
+    }
+}
+
 /* Affichage des différents projets*/
 async function genererProjets() {
     
@@ -73,4 +84,38 @@ function creerProjets (projets)  {
         projetElement.appendChild(nomElement);
     }
 }
+
+let modal = null
+
+const openModal = function (e) {
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute("href"))
+    target.style.display = null
+    target.removeAttribute("aria-hiddent")
+    target.setAttribute("aria-modal", "true")
+    modal = target
+    modal.addEventListener("click", closeModal)
+    modal.querySelector(".fa-xmark").addEventListener("click", closeModal)
+    modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+}
+
+const closeModal = function (e) {
+    e.preventDefault()
+    modal.style.display = "none"
+    modal.setAttribute("aria-hidden", "true")
+    modal.removeAttribute("aria-modal")
+    modal.removeEventListener("click", closeModal)
+    modal.querySelector(".fa-xmark").removeEventListener("click", closeModal)
+    modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
+    modal = null
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation()
+}
+
+document.querySelectorAll(".js-modal").forEach(a => {
+    a.addEventListener("click", openModal)
+    
+})
 
